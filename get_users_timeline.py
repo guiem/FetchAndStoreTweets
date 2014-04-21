4c5eea05e5994cbee67195111b11b0a487544894
guiem <g@guiem.info>
2014-04-21 13:04:02 +0200

    added GAP seconds to determine whether we update a user or not

--- get_users_timeline.py
+++ get_users_timeline.py
@@ -61,14 +61,17 @@ def get_timeline(screen_name, since_id = False):
 users_processed = 0
 users_updated = 0
 for user in users.find():
-    if 'since_id' in user.keys():
-        users_updated += 1
-    since_id = get_timeline(user['screen_name'], since_id = 'since_id' in user.keys() and user['since_id'])
-    if since_id:
-        users.update({"screen_name": user['screen_name']}, {"$set": {"processed": datetime.datetime.utcnow(),"since_id":since_id}})
+    if user['processed'] != 'no' and user['processed'] > (datetime.datetime.utcnow() -  datetime.timedelta(seconds=UPDATE_GAP_SECONDS)): # if user has been processed in the past GAP seconds we do nothing
+        print 'Skipping user {0}'.format(user['screen_name'])
     else:
-        users.update({"screen_name": user['screen_name']}, {"$set": {"processed": datetime.datetime.utcnow()}})
-    users_processed += 1
-    print "{0}'s timline has been processed".format(user['screen_name'])
+        if 'since_id' in user.keys():
+            users_updated += 1
+        since_id = get_timeline(user['screen_name'], since_id = 'since_id' in user.keys() and user['since_id'])
+        if since_id:
+            users.update({"screen_name": user['screen_name']}, {"$set": {"processed": datetime.datetime.utcnow(),"since_id":since_id}})
+        else:
+            users.update({"screen_name": user['screen_name']}, {"$set": {"processed": datetime.datetime.utcnow()}})
+        users_processed += 1
+        print "{0}'s timline has been processed".format(user['screen_name'])
 print 'Total users processed {0}. Users updated: {1}'.format(users_processed, users_updated)
 
